98c5dac233cad44eb5638b1146931e5143382e45
Jared <jaredlll08@gmail.com>
2016-08-04 02:36:04 +0200

    made the power monitor not tick

--- src/main/java/getfluxed/fluxedcrystals/tileentities/greenhouse/monitor/TileEntityPowerMonitor.java
+++ src/main/java/getfluxed/fluxedcrystals/tileentities/greenhouse/monitor/TileEntityPowerMonitor.java
@@ -1,14 +1,10 @@
 package getfluxed.fluxedcrystals.tileentities.greenhouse.monitor;
 
 import getfluxed.fluxedcrystals.tileentities.greenhouse.TileEntityMultiBlockComponent;
-import net.minecraft.util.ITickable;
 
 /**
  * Created by Jared on 4/25/2016.
  */
-public class TileEntityPowerMonitor extends TileEntityMultiBlockComponent implements ITickable{
+public class TileEntityPowerMonitor extends TileEntityMultiBlockComponent {
 
-    @Override
-    public void update() {
-    }
 }
